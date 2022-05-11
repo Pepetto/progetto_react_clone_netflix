fab44bbdfcdf7d850c9cc9dc1671b653833c13ac
Erik Metohu <erikmetohu26@gmail.com>
2022-05-11 17:09:40 +0200

    Aggiunto componente header

--- progetto_netflix/src/App.js
+++ progetto_netflix/src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import MovieCard from "./components/MovieCard/MovieCard.component";
 import NavBar from "./components/NavBar/NavBar.component";
+import Header from "./components/Header/Header.component";
 
 function App() {
   return (
     <div className="App">
+      
       <NavBar/>
+      <Header/>
       <MovieCard />
     </div>
   );
