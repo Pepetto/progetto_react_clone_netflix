16ad7b9b169832e78e77eae6d1135b868b1eb4ef
Manfredi Ruggero <manfrediruggero@gmail.com>
2022-05-11 17:02:57 +0200

    aggiunto componente navbar

--- progetto_netflix/src/App.js
+++ progetto_netflix/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MovieCard from "./components/MovieCard/MovieCard.component";
+import NavBar from "./components/NavBar/NavBar.component";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <MovieCard />
     </div>
   );
---
+++ progetto_netflix/src/components/NavBar/NavBar.component.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+export default function NavBar () {
+    return  <>
+                <h1>NavBar</h1>
+            </>
+}
