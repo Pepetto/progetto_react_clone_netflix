441e5d1886b5699034ffe17f1f5affc0eda1540a
Enrico <enricoricci006@.com>
2022-05-16 11:58:26 +0200

    Aggiunti campi login

--- progetto_netflix/src/pages/Login/Login.page.jsx
+++ progetto_netflix/src/pages/Login/Login.page.jsx
@@ -4,6 +4,25 @@ export default function Login() {
   return (
     <div>
       <h2>LOGIN</h2>
+      <div class="container">
+        <div class="title">CONTATTAMI</div>
+        <form action="Connect.php" method="post">
+            <div class="user-details">
+                
+                <div class="input-box">
+                 <label for="Email" class="details">Email</label>
+                 <input type="text"placeholder="marcorossi@email.com" required name="Email"/>
+                </div>
+                <div class="input-box">
+                 <label for="Password" class="details">Password</label>
+                 <input type="password"placeholder="password" required name="Password"/>
+                </div>
+                <div class="button">
+                    <input type="button" value="Invia"/>
+                </div>
+            </div>
+        </form>
+    </div>
     </div>
   );
 }
