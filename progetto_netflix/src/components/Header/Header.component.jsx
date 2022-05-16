a3249dd5aaf5ccc10f4b827365c310cf0230f059
Manfredi Ruggero <manfrediruggero@gmail.com>
2022-05-16 11:30:36 +0200

    sistemata landing page

--- progetto_netflix/src/components/Header/Header.component.jsx
+++ progetto_netflix/src/components/Header/Header.component.jsx
@@ -6,26 +6,21 @@ export default function Header () {
     return (
             <>
                 <Container fluid> 
-                    <Row className="header-image altezza"> 
+                    <Row className="header-image altezza align-items-end  text-start"> 
                         <Col> 
                             <Row className="mt-3" >
-                                <Col className="d-flex justify-content-start ">
-                                    <img src="https://react-bootstrap.github.io/logo.svg" width="30" height="30" className="d-inline-block align-top" alt="React Bootstrap logo"/>
-                                </Col>
-                                <Col className="d-flex justify-content-end">
-                                    <Link className="btn btn-danger w-25" to="/login">Login</Link>
-                                </Col>
-                            </Row>
-                            <Card.Body className="header-color">
-                            <div className="mt-5 bg bg-dark bordo">
+                                <img className="img-fluid" src="" alt="QUALCOSA" />
                                 <div>
-                                    <h1 >Film, serie TV e tanto altro. Senza limiti.</h1>
-                                </div>  
-                                <div>
-                                    <h2 className="mt-3">Guarda ciò che vuoi ovunque. Disdici quando vuoi.</h2>
+                                    <h1>Film, serie TV e tanto altro. Senza limiti.</h1>
                                 </div>
                                 <div>
-                                    <h3 className="mt-3">Vuoi guardare Netflix? Inserisci l'indirizzo email per abbonarti o riattivare il tuo abbonamento.</h3>
+                                    <h2 className="mt-5">Guarda ciò che vuoi ovunque. Disdici quando vuoi.</h2>
+                                </div>
+                            </Row>
+                            <Card.Body className="header-color">
+                            <div className="mt-5">
+                                <div>
+                                    <h3 className="mt-5">Vuoi guardare Netflix? Inserisci l'indirizzo email per abbonarti o riattivare il tuo abbonamento.</h3>
                                 </div>
                                 <div>
                                     <input type='e-mail' className="w-25" ></input>
@@ -36,14 +31,14 @@ export default function Header () {
                         </Col>
                     </Row>
                     <Row className="mt-5 pt-5 altezza">
-                        <Col className="prova">
-                            {/* <img src="/src/img/35228.jpg" alt="QUALCOSA" /> */}
+                        <Col>
+                            <img  className="img-fluid" src="../../img/35228.jpg" alt="QUALCOSA" />
                         </Col>
-                        <Col className="ms-5">
+                        <Col className="text-start ms-5 fs-4">
                             <div>
                                 <h2>Guarda come, quando e con chi vuoi</h2>
                             </div>
-                            <div className="text-start">
+                            <div className=" mt-5">
                                 <div>
                                     <p>Organizza una visone di gruppo con Groupwatch. Metti in pausa,riavvolgi e interagisci con un massimo di sei persone. Per partecipare è necessario un abbonamento attivo.</p>
                                 </div>
@@ -66,19 +61,19 @@ export default function Header () {
                     <Row xxl={4} xl={4} lg={4} md={2} sm={1} xs={1} className="mt-5">
                         
                         <Col>
-                        <img src="" alt="TV" />
+                        <img className="img-fluid" src="../../img/original.png" alt="TV" />
                            <h3>Amazon Fire TV Dispositivi con Android TV Apple TV Chromecast TV LG Samsung </h3>
                         </Col>
                         <Col>
-                            <img src= "../../img/original.png" alt="PC" />
+                            <img className="img-fluid" src= "../../img/original1.png" alt="PC" />
                             <h3>Chrome OS MacOS PC Windows</h3>
                         </Col>
                         <Col>
-                            <img src="../../img/original.png" alt="Tablet" />
+                            <img className="img-fluid" src="../../img/original2.png" alt="Tablet" />
                             <h3>Tablet Amazon Fire Telefoni e tablet Android iPhone e iPad</h3>
                         </Col>
                         <Col>
-                            <img src="img/original.png" alt="Consol" />
+                            <img className="img-fluid" src="img/original3.png" alt="Consol" />
                             <h3>PS4 PS5 Xbox One Xbox Series X Xbox Series S</h3>
                         </Col>
                     </Row>
