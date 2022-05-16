16810f294a4df27e7bd6ea5fcae7383b281173dc
Erik Metohu <erikmetohu26@gmail.com>
2022-05-16 15:41:48 +0200

    Aggiunto Login

---
+++ progetto_netflix/src/components/Login/Login.component.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import Login from "./Login.css";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Login () {
+    return (
+        <>
+         <Container>
+             <Row>
+                 <Col>
+                 <div class="filter"></div>
+                    <div class="container">
+                        <div class="title">REGISTRATI</div>
+                        <form method="post">
+                            <div class="user-details">
+                                <div class="input-box">
+                                    <label for="text" class="details">User</label>
+                                    <input type="text" placeholder="User"/>
+                                </div>
+                                <div class="input-box">
+                                <label for="Email" class="details">Email</label>
+                                <input type="email" placeholder="marcorossi@email.com" />
+                                </div>
+                                <div class="input-box">
+                                <label class="details">Password</label>
+                                <input type="password"placeholder="Password"/>
+                                </div>
+                                <div class="button">
+                                    <input type="button" value="Invia"/>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                 </Col>
+             </Row>
+         </Container>
+        </>
+    )
+
+}
