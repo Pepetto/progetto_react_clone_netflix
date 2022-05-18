d21a6e32b602a59352fec4fc82f63414fa5731f9
Manfredi Ruggero <manfrediruggero@gmail.com>
2022-05-18 09:36:49 +0200

    Fatta pagina di Login, si deve fare la stessa page per la register

--- progetto_netflix/src/components/Login/Login.component.jsx
+++ progetto_netflix/src/components/Login/Login.component.jsx
@@ -6,30 +6,33 @@ import { Link } from "react-router-dom";
 export default function Login () {
     return (
         <>
-         <Container fluid className="sfondologin">
-             <Row>
-                 <Col className="d-flex justify-content-center mt-5">
-                    <Card className="cardLogin">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" />
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </Card>
-                 </Col>
-             </Row>
-         </Container>
+            <Container fluid className="sfondologin">
+                 <Row className="justify-content-center">
+                     <Col className="d-flex justify-content-center mt-5">
+                        <Card className="cardLogin">
+                            <Card.Title>
+                                <h1 className="mt-3">Accedi</h1>
+                            </Card.Title>
+                            <Card.Body className=" d-flex justify-content-center mt-3" >
+                                <form className="w-50">
+                                    <div className="mb-3">
+                                        <label className="form-label">Email address</label>
+                                        <input type="email" className="form-control" />
+                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label  className="form-label">Password</label>
+                                        <input type="password" className="form-control" />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                </form>
+                            </Card.Body>
+                            <h5 className="mb-3">Prima volta su Netflex? </h5>
+                            <Link to="" className="mb-5" >Registrati</Link>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 
