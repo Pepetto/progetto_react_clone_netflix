790c347e963e27a402200032afcd2e11d7e74f29
Manfredi Ruggero <manfrediruggero@gmail.com>
2022-05-18 09:53:19 +0200

    aggiunto footer nella Login

--- progetto_netflix/src/pages/Login/Login.page.jsx
+++ progetto_netflix/src/pages/Login/Login.page.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Login from "../../components/Login/Login.component";
+import Footer from "../../components/Footer/Footer.component"
+
 
 export default function LoginPage() {
   return (
    <>
+   <div className="loginColor">
     <Login/>
+    <Footer/>
+    </div>
    </>
   );
 }
